b8f7f9cb853eef482b9638aa605e7fceb2eb2864
Italo <italoaurelior@gmail.com>
2019-07-25 13:29:43 -0300

    ajuste rota tatuadores

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import 'typeface-roboto';
 import 'typeface-roboto-slab';
 // pages for this product
 import Components from "./Components/Components.jsx";
-// import LandingPage from "./LandingPage/LandingPage.jsx";
+ import LandingPage from "./LandingPage/LandingPage.jsx";
 // import ProfilePage from "./ProfilePage/ProfilePage.jsx";
 // import LoginPage from "./LoginPage/LoginPage.jsx";
 
@@ -17,8 +17,8 @@ export default () => (
   <Router history={hist}>
     <Switch>
       {/* <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} /> */}
+      <Route path="/profile-page" component={ProfilePage} />*/}
+      <Route path="/tatuadores" component={LandingPage} /> 
       <Route path="/" component={Components} />
     </Switch>
   </Router>
