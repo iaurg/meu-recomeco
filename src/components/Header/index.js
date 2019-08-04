ebc12ac19a9580ad74e804bc119b9374a6dd0e57
Italo <italoaurelior@gmail.com>
2019-08-04 13:55:26 -0300

    Remove temporario tatuadores

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -44,14 +44,14 @@ const Header = props => (
             Faça parte do projeto
           </a>
         </div>
-        <div>
+        {/* <div>
           <Link
             to="/tatuadores"
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
           >
             Tatuadores Participantes
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   </nav>
