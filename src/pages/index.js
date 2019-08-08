b3585a3425adbae2c61283ee65c4eff07f70e62f
Italo <italoaurelior@gmail.com>
2019-08-07 21:24:14 -0300

    Ajuste tatuadores routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
-import React from "react";
-import {createMemoryHistory} from "history";
-import {Route, Router, Switch} from "react-router-dom";
+import React from 'react'
+import { createMemoryHistory } from 'history'
+import { Route, Router, Switch } from 'react-router-dom'
 
-import "../assets/css/global.css";
-import Tatuadores from "./tatuadores";
-import Inicio from "./inicio";
+import '../assets/css/global.css'
+import Tatuadores from './tatuadores'
+import Inicio from './inicio'
 
-
-let hist = createMemoryHistory();
+const hist = createMemoryHistory()
 
 export default () => (
   <Router history={hist}>
     <Switch>
+      <Route exact path="/" component={Inicio} />
       <Route path="/tatuadores" component={Tatuadores} />
-      <Route path="/" component={Inicio}/>
     </Switch>
   </Router>
-);
+)
--- src/pages/tatuadores.js
+++ src/pages/tatuadores.js
@@ -1,8 +1,9 @@
-import React from "react";
-import TatuadorCard from '../components/TatuadorCard';
-import axios from 'axios';
-import Layout from '../components/layout';
-class Tatuadores extends React.Component{
+import React from 'react'
+import axios from 'axios'
+import TatuadorCard from '../components/TatuadorCard'
+import Layout from '../components/layout'
+
+class Tatuadores extends React.Component {
   state = {
     loading: true,
     error: false,
@@ -26,31 +27,44 @@ class Tatuadores extends React.Component{
         this.setState({ loading: false, error })
       })
   }
-  render(){
 
-  return(
-  <Layout>
-
-      <div className="container mx-auto">
-      <div className=" text-center py-4 lg:px-4 p-2">
-        <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-          <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">👀</span>
-          <span className="font-semibold mr-2 text-left flex-auto">
-            Precisamos de mais tatuadores <a href="" className="underline">Inscreva-se</a>
-          </span>
-          <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+  render() {
+    return (
+      <Layout>
+        <div className="container mx-auto">
+          <div className=" text-center py-4 lg:px-4 p-2">
+            <div
+              className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+              role="alert"
+            >
+              <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+                👀
+              </span>
+              <span className="font-semibold mr-2 text-left flex-auto">
+                Precisamos de mais tatuadores{' '}
+                <a href="" className="underline">
+                  Inscreva-se
+                </a>
+              </span>
+              <svg
+                className="fill-current opacity-75 h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+              </svg>
+            </div>
+          </div>
+          <div>
+            {this.state.loading ? (
+              <p className="text-center">Caaaaaaaaaaaaaaaarregando!</p>
+            ) : (
+              <TatuadorCard data={this.state.data} />
+            )}
+          </div>
         </div>
-      </div>
-      <div>
-      {this.state.loading ? (
-            <p className="text-center">Caaaaaaaaaaaaaaaarregando!</p>
-          ) : (
-            <TatuadorCard data={this.state.data}></TatuadorCard>
-          )}
-      </div>
-      </div>
-  </Layout>
-);
+      </Layout>
+    )
+  }
 }
-}
-export default Tatuadores;
+export default Tatuadores
